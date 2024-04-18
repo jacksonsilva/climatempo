3553da83974ff8f2ffa02d35f4dd9f4f51c2a63f
Jackson Silva <jackson.nascimento@gmail.com>
2024-04-17 22:26:13 -0300

    Parte 3

--- src/main/kotlin/com/estudo/climatempo/service/ClimaServices.kt
+++ src/main/kotlin/com/estudo/climatempo/service/ClimaServices.kt
@@ -1,8 +1,17 @@
 package com.estudo.climatempo.service
 
+import com.estudo.climatempo.controller.request.Clima
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+
 class ClimaServices {
 
     fun enviarParaFila() {
         //criar regra de negocio
+        //converter em json
+
+        val clima = Clima()
+        val mapper = jacksonObjectMapper().writeValueAsString(clima)
+        println(mapper)
+
     }
 }
