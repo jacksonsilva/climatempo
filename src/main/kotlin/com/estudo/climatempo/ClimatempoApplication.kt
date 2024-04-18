ad11a5bffcd62bb3ed713c9046227e93fed6fa31
Jackson Silva <jackson.nascimento@gmail.com>
2024-04-17 21:22:18 -0300

    Inicial

---
+++ src/main/kotlin/com/estudo/climatempo/ClimatempoApplication.kt
@@ -0,0 +1,11 @@
+package com.estudo.climatempo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClimatempoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClimatempoApplication>(*args)
+}
---
+++ src/test/kotlin/com/estudo/climatempo/ClimatempoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.estudo.climatempo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClimatempoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
