3fa7117e313d813b7a5c69cf7a797cbdd2e53dfe
Jackson Silva <jackson.nascimento@gmail.com>
2024-04-17 22:15:20 -0300

    Parte 2

---
+++ src/main/kotlin/com/estudo/climatempo/repository/ClimaRepository.kt
@@ -0,0 +1,7 @@
+package com.estudo.climatempo.repository
+
+class ClimaRepository {
+
+    //Implementar o JPA para gravar em uma base de dados
+    //importar a dependencia do JPA
+}
---
+++ src/main/kotlin/com/estudo/climatempo/service/ClimaServices.kt
@@ -0,0 +1,8 @@
+package com.estudo.climatempo.service
+
+class ClimaServices {
+
+    fun enviarParaFila() {
+        //criar regra de negocio
+    }
+}
